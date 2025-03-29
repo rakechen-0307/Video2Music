ad22437b895d78b8eb38b7932865db7f9d43c901
rakechen-0307 <rakechen168@gmail.com>
2025-03-29 10:12:42 +0800

    add test script

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ input_video = "../samples/videos/ysSxxIqKNN0.mp4"
 input_primer = "C Am F G"
 input_key = "C major"
 
-video2music = Video2music()
+video2music = Video2music(device='cpu')
 output_filename = video2music.generate(input_video, input_primer, input_key)
