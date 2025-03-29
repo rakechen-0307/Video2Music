43391aedd20f8901aa54fe12335072d04bd1c6da
rakechen-0307 <rakechen168@gmail.com>
2025-03-29 10:42:23 +0800

    add test script

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
+import os
 from video2music import Video2music
-
-input_video = "../samples/videos/ysSxxIqKNN0.mp4"
 
 input_primer = "C Am F G"
 input_key = "C major"
-
 video2music = Video2music(device='cpu')
-output_filename = video2music.generate(input_video, input_primer, input_key)
+
+video_dir = "../samples/videos"
+output_dir = "./v2m"
+files = sorted(os.listdir(video_dir))
+for i in range(len(files)):
+    file_id = files[i].split('.')[0]
+    input_video = os.path.join(video_dir, files[i])
+    output_filename = video2music.generate(input_video, input_primer, input_key)
+    os.system("mv {source} {output}".format(
+        source="./output/output.mp4",
+        output=os.path.join(output_dir, f"{file_id}.mp4")
+    ))
